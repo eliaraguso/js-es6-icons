d11a58541e1c14b604fd99398c58e7e7a2b4cb0d
eliaraguso <elia.raguso@gmail.com>
2021-09-13 16:07:21 +0200

    refactoring main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+// Milestone 2
+// Coloriamo le icone per tipo
+// Milestone 3
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
 const icons = [
 	{
 	  name: 'apple-alt',
@@ -109,13 +116,69 @@ const icons = [
 	},
   ];
 
-  // Milestone 1
-  // Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+const colors = {
+    food: "red",
+    animal: "green",
+    beverage: "yellow"
+}
 
-icons.forEach((element) => {
-    document.getElementById("icons").innerHTML += `<div class="card">
-                                                            <i class="${element.family} ${element.prefix}${element.name}"></i>
-                                                            <div class="cardText">${element.name}</div>
-                                                        </div>`
+
+// FUNZIONI
+
+
+// argomenti: array delle icon e il node html in cui stampare le nostre icone
+const printIcons = (arr, container) => {
+    // ciclo su ogni icona e compongo il markup html da iniettare
+arr.forEach((element) => {
+
+    // attrraverso la destrutturazione tirop fuori le proprietà dall'oggetto sottoforma di variabili
+    const {name, family, prefix, color} = element;
+
+    container.innerHTML += 
+    `<div class="card">
+        <i class="${family} ${prefix}${name}" style="color: ${color}"></i>
+        <div class="cardText">${name}</div>
+    </div>`
   }
-  )
+  );
+}
+
+// PROGRAMMA PRINCIPALE
+
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+//   Seleziono il contenitore di icone
+const containerIcons = document.getElementById("icons");
+
+
+// Coloriamo le icone per tipo
+const iconsColored = icons.map(
+    (element) => {
+        return {
+            name: element.name,
+            family : element.family,
+            prefix : element.prefix,
+            category : element.category,
+            color: colors[element.category]
+        };
+    }
+);
+
+console.log(iconsColored);
+printIcons(iconsColored, containerIcons); 
+
+
+//   Milestone 3
+//   Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+//   Creo una option per ogni categoria
+// const iconCategories = [];
+
+// icons.forEach(
+//     (elm) => {
+//         if (iconCategories.includes(elm.category) == false) {
+//             iconCategories.push(elm.category);
+
+//         }        
+//     }
+// );
+
+
